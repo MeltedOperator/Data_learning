4d3ea9fffe3e6b346431fe84aa71f0c26af27ffc
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2025-12-13 17:58:38 +0500

    saving results from each test such as difference between lists and sets for better undertanding how organising data is important when it comes to time and resource management. Not every method works the same, thats why we have to find the most efficient one

--- lab01_experiment.py
+++ lab01_experiment.py
@@ -1,50 +1,59 @@
-import time
-import random
-
-# Генерация данных
-n = 100000
-data = [random.randint(1, n // 2) for _ in range(n)]
-target = random.choice(data)
-
-# TODO: Измерьте время поиска в списке
-start = time.perf_counter()
-target in data
-end = time.perf_counter()
-elapsed_list = end - start
-
-
-# TODO: Измерьте время поиска в множестве
-data_set = set(data)
-start = time.perf_counter()
-found = target in data_set
-time_set = time.perf_counter() - start
-
-# TODO: Выведите результаты
-print("List search:", elapsed_list)
-print("Set  search:", time_set)
-
 # import time
 # import random
 
-# sizes = [1000, 10000, 100000, 300000]
-# results = []
+# # Генерация данных
+# n = 100000
+# data = [random.randint(1, n // 2) for _ in range(n)]
+# target = random.choice(data)
 
-# for n in sizes:
-#     # Генерация данных
-#     data = [random.randint(1, n // 2) for _ in range(n)]
-#     target = random.choice(data)
+# # TODO: Измерьте время поиска в списке
+# start = time.perf_counter()
+# target in data
+# end = time.perf_counter()
+# elapsed_list = end - start
+
+
+# # TODO: Измерьте время поиска в множестве
+# data_set = set(data)
+# start = time.perf_counter()
+# found = target in data_set
+# time_set = time.perf_counter() - start
+
+# # TODO: Выведите результаты
+# print("List search:", elapsed_list)
+# print("Set  search:", time_set)
+
+import time
+import random
+
+sizes = [1000, 10000, 100000, 300000]
+results = []
+
+for n in sizes:
+    # Генерация данных
+    data = [random.randint(1, n // 2) for _ in range(n)]
+    target = random.choice(data)
     
-#     # Измерение поиска в списке
-#     start = time.perf_counter()
-#     found = target in data
-#     time_list = time.perf_counter() - start
+    # Измерение поиска в списке
+    start = time.perf_counter()
+    found = target in data
+    time_list = time.perf_counter() - start
     
-#     # Измерение поиска в множестве
-#     data_set = set(data)
-#     start = time.perf_counter()
-#     found = target in data_set
-#     time_set = time.perf_counter() - start
+    # Измерение поиска в множестве
+    data_set = set(data)
+    start = time.perf_counter()
+    found = target in data_set
+    time_set = time.perf_counter() - start
     
-#     results.append((n, time_list, time_set))
+    results.append((n, time_list, time_set))
     
-#     print(f"n={n:7d} | Список: {time_list:.6f}с | Множество: {time_set:.6f}с")
+    print(f"n={n:7d} | Список: {time_list:.6f}с | Множество: {time_set:.6f}с")
+
+with open('lab01_results.txt', 'w', encoding='utf-8') as f:
+    f.write("Размер | Список (сек) | Множество (сек) | Ускорение\n")
+    f.write("-" * 60 + "\n")
+    for n, t_list, t_set in results:
+        speedup = t_list / t_set if t_set > 0 else float('inf')
+        f.write(f"{n:7d} | {t_list:12.6f} | {t_set:15.6f} | {speedup:9.1f}x\n")
+
+print("Результаты сохранены в lab01_results.txt")

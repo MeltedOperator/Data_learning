331c670fabfbce4313ff1185f62c0e35d9a12dc1
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2025-12-13 15:36:53 +0500

    made a set of numbers and it seems that for some reason random numbers just...work faster than their set idk, next step i will make numbers go with average timer within 5 tries at least

--- lab01_experiment.py
+++ lab01_experiment.py
@@ -2,17 +2,22 @@ import time
 import random
 
 # Генерация данных
-n = 100000
+n = 10000000
 data = [random.randint(1, n // 2) for _ in range(n)]
 target = random.choice(data)
 
 # TODO: Измерьте время поиска в списке
 start = time.perf_counter()
 target in data
-print(f"Size={n}, avg_time={start} s")
-# ..
+t_list = time.perf_counter() - start
+
 
 # TODO: Измерьте время поиска в множестве
-# ...
+s = set(data)
+start = time.perf_counter()
+target in s
+t_set = time.perf_counter() - start
 
 # TODO: Выведите результаты
+print("List search:", t_list)
+print("Set  search:", t_set)

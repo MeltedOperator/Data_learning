e9b657109fc65516059cc26cb32879f8eba61227
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2025-12-13 17:34:54 +0500

    testing different list sizes such as thousands, hundred of thousands and etc. Turns out, with it takes more time the more elements there are in list, who could've thought

--- lab01_experiment.py
+++ lab01_experiment.py
@@ -14,12 +14,37 @@ elapsed_list = end - start
 
 
 # TODO: Измерьте время поиска в множестве
-s = set(data)
+data_set = set(data)
 start = time.perf_counter()
-target in s
-end = time.perf_counter()
-elapsed_set = end - start
+found = target in data_set
+time_set = time.perf_counter() - start
 
 # TODO: Выведите результаты
 print("List search:", elapsed_list)
-print("Set  search:", elapsed_set)
+print("Set  search:", time_set)
+
+# import time
+# import random
+
+# sizes = [1000, 10000, 100000, 300000]
+# results = []
+
+# for n in sizes:
+#     # Генерация данных
+#     data = [random.randint(1, n // 2) for _ in range(n)]
+#     target = random.choice(data)
+    
+#     # Измерение поиска в списке
+#     start = time.perf_counter()
+#     found = target in data
+#     time_list = time.perf_counter() - start
+    
+#     # Измерение поиска в множестве
+#     data_set = set(data)
+#     start = time.perf_counter()
+#     found = target in data_set
+#     time_set = time.perf_counter() - start
+    
+#     results.append((n, time_list, time_set))
+    
+#     print(f"n={n:7d} | Список: {time_list:.6f}с | Множество: {time_set:.6f}с")

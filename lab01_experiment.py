84d61824671a56b753c112a2cf74b556c5f597ca
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2025-12-13 16:23:45 +0500

    seems like set of elements will be unefficient with huge amount of numbers such as millions or even billions in comparison with unogranized data which is weird. But with hundred of thousand set is working much faster than usual list

--- lab01_experiment.py
+++ lab01_experiment.py
@@ -2,22 +2,24 @@ import time
 import random
 
 # Генерация данных
-n = 10000000
+n = 100000
 data = [random.randint(1, n // 2) for _ in range(n)]
 target = random.choice(data)
 
 # TODO: Измерьте время поиска в списке
 start = time.perf_counter()
 target in data
-t_list = time.perf_counter() - start
+end = time.perf_counter()
+elapsed_list = end - start
 
 
 # TODO: Измерьте время поиска в множестве
 s = set(data)
 start = time.perf_counter()
 target in s
-t_set = time.perf_counter() - start
+end = time.perf_counter()
+elapsed_set = end - start
 
 # TODO: Выведите результаты
-print("List search:", t_list)
-print("Set  search:", t_set)
+print("List search:", elapsed_list)
+print("Set  search:", elapsed_set)

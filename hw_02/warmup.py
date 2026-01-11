f89fe4de94eeb495e75fefca7474e328e756de6c
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-01-11 23:57:31 +0500

    Warmup.py - put comments where needed and added description to all functions, so now you can know what they are doing without checking directly

--- hw_02/warmup.py
+++ hw_02/warmup.py
@@ -1,7 +1,13 @@
 
 def reverse_string(string: str) -> str:
+
+    """эта функция переворачивает строку наоборот без использования
+    s[::-1], reversed()"""
+
     s = len(string)
+
     stack = []
+
     for i in range(0, s, 1):
         stack.append(string[i])
 
@@ -9,32 +15,47 @@ def reverse_string(string: str) -> str:
     
     for i in range(0, s, 1):
         string+=stack.pop()
+
     return string
 
-print(reverse_string("olleh"))
 
 class QueueFromStacks:
+
     def __init__(self):
+
+        """инициализируем два стека - stack_in и stack_out"""
+
         self.stack_in = []
         self.stack_out = []
-    
+
     def enqueue(self, item):
+
+        """добавляем элементы в stack_in"""
+        
         self.stack_in.append(item)
-        pass
-    
+        
+
     def dequeue(self):
+
+        """в этой функции мы перекладываем элементы из stack_in
+        в stack_out при условии если stack_out пустой"""
+
         # Если stack_out пуст, перекладываем из stack_in
+
         if len(self.stack_out) == 0:
             while self.stack_in:
                 self.stack_out.append(self.stack_in.pop())
 
     def show(self):
-        print(self.stack_in)
-        print(self.stack_out)
-        
+        return self.stack_in, self.stack_out
+    
+
 queue = QueueFromStacks()
+
 queue.enqueue(1)
 queue.enqueue(3)
 queue.enqueue(5)
 queue.dequeue()
-queue.show()
+print(queue.show())
+
+print(reverse_string("olleh"))

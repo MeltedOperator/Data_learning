ac8fef2499eda7dac8d5c95ff914a75db6f524ee
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-01-12 00:25:02 +0500

    Routes.py - by adding two more lines - num10 and num11 (in which code erases all commas, brackets) made table look more appealing. Also added | those sticks because they are cool

--- hw_02/routes.py
+++ hw_02/routes.py
@@ -1,18 +1,20 @@
-import csv
+import csv 
 
 def load_routes(filename="routes.csv") -> dict:
     """Возвращает {route_id: {"name": str, "stops": list}}"""
     routes = []
-    with open(filename, newline="",encoding="utf-8") as f:
+    with open(filename, newline="", encoding="utf-8") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            cities = row["route_name"]
-            routes.append({
-                "id": row["route_id"],
-                "route_name": cities
-            })
-        return routes
-    pass
+            route_id = row["route_id"]
+            cities = [city.strip() for city in row["stops"].split(",")]
+            city_names = [city.strip() for city in row["route_name"].split(",")]
+            names = ",".join(city_names)
+            # route_name = "-".join(cities)
+            stops = ",".join(cities)
+            cities_info = f"{route_id} | {names} | {stops}"
+            routes.append(cities_info)
+            print(f"{route_id} | {names} | {stops}")
 
 def find_route(routes: dict, route_id: int) -> dict | None:
     """O(1) поиск. None если не найден."""

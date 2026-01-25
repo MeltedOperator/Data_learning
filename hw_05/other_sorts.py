c5df2749ee18efc0dac60b678996003e0cb8ce93
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-01-26 00:14:40 +0500

    Сортировка вставкой - сортировка, где первоначальный элемент считается отсортированным, а все остальные элементы мы вставляем в нужное место по их размеру

--- hw_05/other_sorts.py
+++ hw_05/other_sorts.py
@@ -24,4 +24,12 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
         if not swapped: break
+    return arr 
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]; j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]; j -= 1
+        arr[j + 1] = key
     return arr

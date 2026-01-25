04a0a76b0fbfd780d996ba7bbd2cae1e27e184a3
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-01-25 18:06:25 +0500

    добавил основу для создания и дальнейшей работы с кучей(heap)

---
+++ hw_05/heap_sort.py
@@ -0,0 +1,9 @@
+def heapify(arr, n, i):
+    """Просеивание вниз для max-heap."""
+    # Ваша реализация
+    pass
+
+def heap_sort(arr):
+    """Сортировка кучей (in-place, возвращает arr)."""
+    # Ваша реализация
+    pass

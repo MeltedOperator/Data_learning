290fbfb3464dd596a86472649121c6be6280d2f2
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-01-26 00:05:04 +0500

    добавил тесты для heap_sort чтобы посмотреть как все работает. Если запустить код, то тесты будут работать даже если массив будет пуст/в нем будет один элемент/в нем будут случайные данные

--- hw_05/heap_sort.py
+++ hw_05/heap_sort.py
@@ -19,3 +19,23 @@ def heap_sort(arr):
         heapify(arr, i, 0)
     return arr
 
+def test_heap_sort():
+    # Пустой массив
+    assert heap_sort([]) == []
+    
+    # Один элемент
+    assert heap_sort([42]) == [42]
+    
+    # Уже отсортирован
+    assert heap_sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    
+    # Обратный порядок
+    assert heap_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+    
+    # Случайные данные
+    data = [random.randint(0, 10000) for _ in range(1000)]
+    assert heap_sort(data.copy()) == sorted(data)
+    
+    print("✓ Все тесты пройдены!")
+
+test_heap_sort()
